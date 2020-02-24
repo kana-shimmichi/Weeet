66fc6e7ae9464a4dcc0e2e605dcc305717bfdbf7
ryuji <ryuji.yasu@gmail.com>
2020-02-24 21:28:55 +0900

    good

--- diagnotis/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DiagnotisConfig(AppConfig):
-    name = 'diagnotis'
--- my_page/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from home.models import Order
-
-
-class OrderForm(forms.ModelForm):
-
-    class Meta:
-        model = Order
-        fields = ('name', 'email','body','picture')
