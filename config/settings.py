4edb91e3825173dafedf81a5bf23ba2b1a81852d
ryuji <ryuji.yasu@gmail.com>
2020-04-03 20:16:54 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -122,12 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/mysite/static'
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT ='/var/www/mysite/media'
+if DEBUG:
+    pass
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/var/www/mysite/static'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT ='/var/www/mysite/media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
